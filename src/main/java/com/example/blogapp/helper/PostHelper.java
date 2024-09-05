82fb2e8ea4c7575a12d73087179f0601bdb26aea
Nona <1208ny0324@gmail.com>
2024-09-05 09:33:02 +0900

    Restore PostHelper.java

---
+++ src/main/java/com/example/blogapp/helper/PostHelper.java
@@ -0,0 +1,39 @@
+package com.example.blogapp.helper;
+
+import com.example.blogapp.entity.Post;
+import com.example.blogapp.form.PostForm;
+
+/** Postヘルパー */
+public class PostHelper {
+	/**
+	 * Postへの変換
+	 * @param form
+	 * @return post オブジェクト
+	 */
+	public static Post convertPost(PostForm form) {
+		Post post = new Post();
+		post.setId(form.getId());
+		post.setTitle(form.getTitle());
+		post.setDescription(form.getDescription());
+		/** 画像のパスをセット */
+		post.setImagePath(form.getImagePath());
+		return post;
+	}
+
+	/**
+	 * PostFormへの変換
+	 * @param post
+	 * @return form (postの値をセット)
+	 */
+	public static PostForm convertPostForm(Post post) {
+		PostForm form = new PostForm();
+		form.setId(post.getId());
+		form.setTitle(post.getTitle());
+		form.setDescription(post.getDescription());
+		/** 画像のパスをセット */
+		form.setImagePath(post.getImagePath());
+		// 更新画面設定
+		form.setIsNew(false);
+		return form;
+	}
+}
