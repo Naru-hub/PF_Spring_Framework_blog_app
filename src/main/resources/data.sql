0c919b1104bf9e4616e26bec9255151e2e82f04b
Nona <1208ny0324@gmail.com>
2024-09-30 10:41:34 +0900

    初期データの設定を変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,23 @@
 -- userのデータ
+-- 1人目のデータ
 INSERT INTO auth_users (username, password, email, created_at, updated_at)
-VALUES ('testUser', '$2a$10$bnXFUQawIzzLGELILFMjkego0N50DKS1J/XVcHBJ2YO3I3Zh8yMhS', 'test@test.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('テスト　太郎', '$2a$10$bnXFUQawIzzLGELILFMjkego0N50DKS1J/XVcHBJ2YO3I3Zh8yMhS', 'test@test.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+UPDATE auth_users 
+SET user_image_path = '/uploads/default_img/user1_default.jpg' where id = 1;
 
+-- 2人目のデータ
+INSERT INTO auth_users (username, password, email, created_at, updated_at)
+VALUES ('テスト　花子', '$2a$10$bnXFUQawIzzLGELILFMjkego0N50DKS1J/XVcHBJ2YO3I3Zh8yMhS', 'taro@test.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+UPDATE auth_users 
+SET user_image_path = '/uploads/default_img/user2_default.jpg' where id = 2;
+
+-- 投稿のデータ
 -- 1件目のデータ
 INSERT INTO posts (user_id, title, description, image_path, created_at, updated_at)
-VALUES ('1', 'スッキリJava', '優しい解説で分かりやすい', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('1', 'スッキリJava', '優しい解説で分かりやすい。オブジェクト指向入門に最適です！！', '/uploads/default_img/java_default.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- 2件目のデータ
 INSERT INTO posts (user_id, title, description, image_path, created_at, updated_at)
-VALUES ('1', 'スッキリSQL', '初心者でもわかりやすい構成', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('2', 'ソフトウェアエンジニアリングの新人研修', 'webの大まかな流れがつかめ、開発がどのように進んでいくかを知るのによかった。　新人の時に読むことをおすすめします。', '/uploads/default_img/it_basic_default.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- 3件目のデータ
 INSERT INTO posts (user_id, title, description, image_path, created_at, updated_at)
-VALUES ('1', 'プロジェクトマネジメント', 'webの大まかな流れがつかめる', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('1', 'スッキリSQL', 'SQLが初めての方に特にお勧めです。入門として非常にわかりやすい構成です。', '/uploads/default_img/sql_default.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
