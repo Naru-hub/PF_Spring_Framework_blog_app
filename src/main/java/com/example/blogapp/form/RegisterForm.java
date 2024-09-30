459be442781b61033b1dfe5d02cc2a1e06726dd1
Nona <1208ny0324@gmail.com>
2024-09-30 13:44:19 +0900

    ユーザー登録時のパスワードの文字数を8文字以上に設定

--- src/main/java/com/example/blogapp/form/RegisterForm.java
+++ src/main/java/com/example/blogapp/form/RegisterForm.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -18,9 +19,11 @@ public class RegisterForm {
 	
 	/** パスワード */
 	@NotEmpty(message = "パスワードは必須です。")
+	@Size(min = 8, message = "パスワードは{min}文字以上を入力してください" )
 	private String password;
 	
 	/** 確認用パスワード */
+	@NotEmpty(message = " 確認用パスワードは必須です。")
 	private String confirmPassword;
 	
 	/** メールアドレス */
